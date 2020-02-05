6cf887a764b511db2797fe445f6d0f62afe5a52e
yuxuewen <8586826@qq.com>
2020-02-06 00:46:24 +0800

    version v1.1.0

--- app.js
+++ app.js
@@ -1,27 +1,6 @@
 'use strict';
 
-const assert = require('assert');
-const SMSClient = require('@alicloud/sms-sdk');
-
+const aliyunSms = require('./lib/aliyunSms');
 module.exports = app => {
-
-  const { accessKeyId, secretAccessKey, smsApiEndpoint, baseApiEndpoint, regionId, mnsVpc } = app.config.aliyunSms || {};
-
-  let options = {};
-
-  assert(accessKeyId && secretAccessKey , '[egg-aliyun-sms] must set `accessKeyId` and `secretAccessKey` in config files.');
-
-  Object.assign(options,{accessKeyId, secretAccessKey});
-
-  if (mnsVpc && mnsVpc.vpc) {
-      assert(smsApiEndpoint && baseApiEndpoint && regionId,
-          'if you use vpc , [egg-aliyun-sms] must set `smsApiEndpoint` and `baseApiEndpoint` and `regionId` and `mnsVpc` in config files.');
-
-      Object.assign(options,{smsApiEndpoint, baseApiEndpoint, regionId});
-  }
-
-  app.coreLogger.info('[egg-aliyun-sms] begin');
-
-  app.smsClient = new SMSClient(options);
-
+  aliyunSms(app);
 };
--- test/fixtures/apps/aliyun-sms-test/app/controller/home.js
+++ test/fixtures/apps/aliyun-sms-test/app/controller/home.js
@@ -4,6 +4,14 @@ const Controller = require('egg').Controller;
 
 class HomeController extends Controller {
   async index() {
+    console.log('this.app---create', this.app.aliyunSms.create({
+      accessKeyId: 'LTAI4FiW7ykVpsp1C3M2EBzg',
+      secretAccessKey: 'hZZmISbQzKUucNPe2woxXFYa3aplaP',
+      loginTemplate: 'SMS_161591103',
+      registerTemplate: 'SMS_161596094',
+      resetPasswordTemplate: 'SMS_161591107',
+      replacePhoenTemplate: 'SMS_162140110',
+    }));
     this.ctx.body = 'hi, ' + this.app.plugins.aliyunSms.name;
   }
 }
